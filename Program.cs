629adfdc38ff6ac87e72f0144f7adca9c1c17131
IPIA <57289047+IP1A@users.noreply.github.com>
2024-03-05 23:58:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool startGame;
+
+            Console.WriteLine("Want guess a number? +/-");
+            startGame = Console.ReadLine() == "+";
+
+            if (!startGame) { return; }
+
+            var rng = new Random(0);
+
+            int number = Convert.ToInt32(rng.Next(0, 100));
+
+            Console.WriteLine("I made a number from 0 to 100.");
+
+            while (true)
+            {
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess > number)
+                {
+                    Console.WriteLine("Smaller.");
+                }
+                else if (guess < number) {
+                    Console.WriteLine("Bigger.");
+                }
+                else
+                {
+                    Console.WriteLine("You guess it!");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+        }
+    }
+}
